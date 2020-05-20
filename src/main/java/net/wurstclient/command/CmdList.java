72728ac0c17d4ba24d4f285c73219dc41d931a55
ThisTestUser <ThisTestUser@users.noreply.github.com>
2020-05-20 17:08:30 -0400

    PlaceSlot

--- src/main/java/net/wurstclient/command/CmdList.java
+++ src/main/java/net/wurstclient/command/CmdList.java
@@ -44,6 +44,7 @@ public final class CmdList
 	public final LeaveCmd leaveCmd = new LeaveCmd();
 	public final ModifyCmd modifyCmd = new ModifyCmd();
 	public final PathCmd pathCmd = new PathCmd();
+	public final PlaceSlotCmd placeSlotCmd = new PlaceSlotCmd();
 	public final PotionCmd potionCmd = new PotionCmd();
 	public final ProtectCmd protectCmd = new ProtectCmd();
 	public final RenameCmd renameCmd = new RenameCmd();
---
+++ src/main/java/net/wurstclient/commands/PlaceSlotCmd.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2014 - 2020 | Alexander01998 | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+import net.wurstclient.util.ChatUtils;
+
+public final class PlaceSlotCmd extends Command
+{
+	public PlaceSlotCmd()
+	{
+		super("placeslot", "Duplicates items in your hand in a specified armor slot.\n"
+			+ "Requires creative mode.",
+			"(head|chest|legs|feet)");
+	}
+
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		if(args.length != 1)
+			throw new CmdSyntaxError();
+		if(!MC.player.abilities.creativeMode)
+			throw new CmdError("Creative mode only.");
+		if(args.length == 1 && args[0].equalsIgnoreCase("head") ||
+			args[0].equalsIgnoreCase("chest") || args[0].equalsIgnoreCase("legs") ||
+			args[0].equalsIgnoreCase("feet"))
+		{
+			int slot = -1;
+			int armorSlot = -1;
+			ItemStack item = null;
+			item = MC.player.inventory.getMainHandStack();
+			if(item == null)
+				throw new CmdError("There is no item in your hand.");
+			switch(args[0].toLowerCase())
+			{
+				case "head":
+					slot = 5;
+					armorSlot = 3;
+					break;
+				case "chest":
+					slot = 6;
+					armorSlot = 2;
+					break;
+				case "legs":
+					slot = 7;
+					armorSlot = 1;
+					break;
+				case "feet":
+					slot = 8;
+					armorSlot = 0;
+					break;
+				default:
+					throw new CmdSyntaxError();
+			}
+			if(!MC.player.inventory.getArmorStack(armorSlot).isEmpty())	
+				throw new CmdError("Item already in slot \"" + args[0] + "\"");
+			if(slot > 8 || slot < 5)
+				throw new CmdError("Armor slot is invaild.");
+			else 
+			{
+				MC.player.inventory.armor.set(armorSlot, item);
+				MC.player.networkHandler.sendPacket(new CreativeInventoryActionC2SPacket(
+					slot, item));
+			}
+			ChatUtils.message("Item placed in slot \"" + args[0] + "\"");
+		}else
+			throw new CmdSyntaxError();
+	}
+}
