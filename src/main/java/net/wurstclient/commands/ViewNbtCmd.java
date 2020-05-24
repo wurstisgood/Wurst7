158562264672298442efa55b6dc480e4039974c8
ThisTestUser <ThisTestUser@users.noreply.github.com>
2020-05-24 12:55:57 -0400

    ViewNBT BlockEntity

--- src/main/java/net/wurstclient/commands/ViewNbtCmd.java
+++ src/main/java/net/wurstclient/commands/ViewNbtCmd.java
@@ -7,7 +7,10 @@
  */
 package net.wurstclient.commands;
 
-import net.minecraft.client.network.ClientPlayerEntity;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.wurstclient.SearchTags;
@@ -16,40 +19,74 @@ import net.wurstclient.command.CmdException;
 import net.wurstclient.command.CmdSyntaxError;
 import net.wurstclient.command.Command;
 import net.wurstclient.util.ChatUtils;
+import net.wurstclient.util.MathUtils;
 
 @SearchTags({"view nbt", "NBTViewer", "nbt viewer"})
 public final class ViewNbtCmd extends Command
 {
 	public ViewNbtCmd()
 	{
-		super("viewnbt", "Shows you the NBT data of an item.", ".viewnbt",
-			"Copy to clipboard: .viewnbt copy");
+		super("viewnbt",
+			"Allows you to see the NBT tags of TileEntities\n"
+				+ "or items in your hand.",
+			".viewnbt print <x> <y> <z>", ".viewnbt copy <x> <y> <z>",
+			".viewnbt tilelist", ".viewnbt inv (print|copy)");
 	}
-	
+
 	@Override
 	public void call(String[] args) throws CmdException
 	{
-		ClientPlayerEntity player = MC.player;
-		ItemStack stack = player.inventory.getMainHandStack();
-		if(stack.isEmpty())
-			throw new CmdError("You must hold an item in your main hand.");
-		
-		CompoundTag tag = stack.getTag();
-		String nbt = tag == null ? "" : tag.asString();
-		
-		switch(String.join(" ", args).toLowerCase())
+		if(args.length == 1 && args[0].equalsIgnoreCase("tilelist"))
 		{
-			case "":
-			ChatUtils.message("NBT: " + nbt);
-			break;
-			
-			case "copy":
-			MC.keyboard.setClipboard(nbt);
-			ChatUtils.message("NBT data copied to clipboard.");
-			break;
-			
-			default:
+			int count = 0;
+			for(BlockEntity be : MC.world.blockEntities)
+			{
+				ChatUtils.message(be.getPos().toString());
+				count++;
+			}
+			ChatUtils.message("Total entities: " + count);
+		}else if(args.length == 4 && (args[0].equalsIgnoreCase("print") || args[0].equalsIgnoreCase("copy")) && 
+			MathUtils.isInteger(args[1]) && MathUtils.isInteger(args[2]) && MathUtils.isInteger(args[3]))
+		{
+			int x = Integer.parseInt(args[1]);
+			int y = Integer.parseInt(args[2]);
+			int z = Integer.parseInt(args[3]);
+			for(BlockEntity be : MC.world.blockEntities)
+			{
+				if(be.getPos().getX() == x && be.getPos().getY() == y &&
+					be.getPos().getZ() == z)
+				{
+					CompoundTag tag = be.toTag(new CompoundTag());
+					if(args[0].equalsIgnoreCase("print"))
+						ChatUtils.message("Data: " + tag.toString());
+					else if(args[0].equalsIgnoreCase("copy"))
+					{
+						MC.keyboard.setClipboard(tag.toString());
+						ChatUtils.message("Data copied to clipboard.");
+					}
+					return;
+				}
+			}
+			throw new CmdError("TileEntity not found at " + x + ", " + y + ", " + z + "!");
+		}else if(args.length == 2 && args[0].equalsIgnoreCase("inv") && (args[1].equalsIgnoreCase("print")
+			|| args[1].equalsIgnoreCase("copy")))
+		{
+			CompoundTag tag = new CompoundTag();
+			ItemStack item = MC.player.inventory.getMainHandStack();
+			if(!item.isEmpty())
+			{
+				item.setTag(tag);
+				if(args[1].equalsIgnoreCase("print"))
+					ChatUtils.message("Data: " + tag.toString());
+				else
+				{
+					Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+						new StringSelection(tag.toString()), null);
+					ChatUtils.message("NBT data copied to clipboard.");
+				}
+			}else
+				ChatUtils.message("You must hold an item in your main hand.");
+		}else
 			throw new CmdSyntaxError();
-		}
 	}
 }
