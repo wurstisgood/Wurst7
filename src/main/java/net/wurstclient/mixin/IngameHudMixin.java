fa4e4dc57e225e70a95f08bb609290bb6d56ab42
ThisTestUser <ThisTestUser@users.noreply.github.com>
2020-06-20 16:21:41 -0400

    NoWither

--- src/main/java/net/wurstclient/mixin/IngameHudMixin.java
+++ src/main/java/net/wurstclient/mixin/IngameHudMixin.java
@@ -10,10 +10,14 @@ package net.wurstclient.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.wurstclient.WurstClient;
 import net.wurstclient.events.GUIRenderListener.GUIRenderEvent;
 
@@ -39,5 +43,17 @@ public class IngameHudMixin extends DrawableHelper
 	{
 		if(WurstClient.INSTANCE.getHax().noPumpkinHack.isEnabled())
 			ci.cancel();
+	}
+	
+	@Redirect(at = @At(value = "INVOKE",
+		target = "Lnet/minecraft/entity/player/PlayerEntity;hasStatusEffect(Lnet/minecraft/entity/effect/StatusEffect;)Z"),
+		method = {
+			"renderStatusBars()V"})
+	private boolean hasEffect(PlayerEntity e, StatusEffect effect)
+	{
+		if(effect == StatusEffects.POISON && WurstClient.INSTANCE.getHax().noWitherHack.isEnabled()
+			&& e.hasStatusEffect(StatusEffects.WITHER))
+			return true;
+		return e.hasStatusEffect(effect);
 	}
 }
